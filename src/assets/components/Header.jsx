18b4d7daba13f7f746b743700edec6de99ceed6e
v-lahutik <v.lahutik@gmail.com>
2024-08-26 14:09:58 +0200

    made some changes on resposivness

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Header() {
   return (
     <>
-    <h1 className="m-6 font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl">
-    <span class="md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-300 from-sky-400">
-    CashFlow Control 
-    </span>
+    <h1 className="m-6 font-extrabold text-gray-900 dark:text-white text-4xl md:text-5xl lg:text-6xl">
+  <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-300 from-sky-400">
+    CashFlow Control
+  </span>
     
   </h1>
   <p className="m-6 italic text-gray-500 dark:text-white md:text-3xl lg:text-3xl">
